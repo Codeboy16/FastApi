9846a820268f554a996237e33b7a6d2f98c98675
CodingBoy16 <rs1644818@gmail.com>
2025-12-28 16:08:23 +0530

    Added CORS To Accept All REQ

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ from pydantic import BaseModel, EmailStr,Field,conint
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pathlib import Path
+from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.mount("/images", StaticFiles(directory="images"), name="images")
 
+#CROS Allow All Req
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins (useful for dev)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 # GET REQUEST
 @app.get("/")
 async def read_root():
     return ({"Message": "Hello Dev! Fast Api Is Working", "Status": "Success",
-             "Api Routes": ["/student", "/teacher", "/search"]})
+             "Api Routes": ["/student", "/teacher", "findPg/search?location=gangtok&budget=10000","/findPg/topBestPg"]})
 
 
 
